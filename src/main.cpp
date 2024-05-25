ee756dcabbeb4c26ed973e3223b35bd1c3a5dc2d
jzolee <106175689+jzolee@users.noreply.github.com>
2024-05-25 09:06:34 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -8,9 +8,10 @@
 
 void setup()
 {
+    startMutex = xSemaphoreCreateMutex();
     xTaskCreatePinnedToCore(comm_task, "comm_task", 2048, NULL, 2, &comm_task_handle, 0);
     xTaskCreatePinnedToCore(watchdog_task, "watchdog_task", 2048, NULL, 1, NULL, 0);
-    vTaskDelay(200);
+    xSemaphoreTake (startMutex, portMAX_DELAY);
     xTaskCreatePinnedToCore(stepgen_task, "stepgen_task", 2048, NULL, 1, NULL, 1);
     vTaskDelete(NULL);
 }
